312988d5dc358afc4e75af54865b8deb1c6b9d5f
Chia-Hsien Lin <f5822538@gmail.com>
2022-07-12 03:29:11 +0800

    Update ImportDbService.cs

--- DbAdm/Services/ImportDbService.cs
+++ DbAdm/Services/ImportDbService.cs
@@ -22,17 +22,17 @@ namespace DbAdm.Services
             var db = new Db();
             Db dbSrc = null;
             var project = await db.GetJsonAsync(string.Format(@"
-select DbName, ConnectStr, FromTmpTable
-from dbo.Project
-where Id='{0}'
-", projectId));
+            select DbName, ConnectStr, FromTmpTable
+            from dbo.Project
+            where Id='{0}'
+            ", projectId));
 
             if (project == null)
             {
                 result.ErrorMsg = "Project no row";
                 goto lab_exit;
             }
-            else if(_Object.IsEmpty(project["DbName"]) || _Object.IsEmpty(project["ConnectStr"]))
+            else if (_Object.IsEmpty(project["DbName"]) || _Object.IsEmpty(project["ConnectStr"]))
             {
                 result.ErrorMsg = "Project.DbName or ConnectStr is empty.";
                 goto lab_exit;
@@ -62,20 +62,20 @@ where Id='{0}'
 
             #region 2.create temp table: #tmpTable, #tmpColumn
             await db.ExecSqlAsync(@"
-CREATE TABLE #tmpTable(
-    Code varchar(100) NOT NULL primary key,
-    Note nvarchar(255) NULL
-);
-CREATE TABLE #tmpColumn(
-	Code varchar(100) NOT NULL,
-	TableCode varchar(100) NULL,
-	DataType varchar(20) NOT NULL,
-	Nullable bit NOT NULL,
-	DefaultValue varchar(100) NULL,
-	Sort smallint NOT NULL,
-	Note nvarchar(100) NULL
-);
-CREATE NONCLUSTERED	INDEX ix_tmpColumn ON #tmpColumn (Code);");
+            CREATE TABLE #tmpTable(
+                Code varchar(100) NOT NULL primary key,
+                Note nvarchar(255) NULL
+            );
+            CREATE TABLE #tmpColumn(
+            	Code varchar(100) NOT NULL,
+            	TableCode varchar(100) NULL,
+            	DataType varchar(20) NOT NULL,
+            	Nullable bit NOT NULL,
+            	DefaultValue varchar(100) NULL,
+            	Sort smallint NOT NULL,
+            	Note nvarchar(100) NULL
+            );
+            CREATE NONCLUSTERED	INDEX ix_tmpColumn ON #tmpColumn (Code);");
             #endregion
 
             #region 3.write #tmpTable(from Information_Schema.Tables)
@@ -130,14 +130,14 @@ CREATE NONCLUSTERED	INDEX ix_tmpColumn ON #tmpColumn (Code);");
             #region 5.insert/update dbo.Table from #tmpTable
             //get rows for insert new 
             var tables = await db.GetJsonsAsync(string.Format(@"
-select Code, Note
-from #tmpTable
-where Code not in (
-    select Code 
-    from dbo.[Table]
-    where ProjectId='{0}'
-)
-", projectId));
+            select Code, Note
+            from #tmpTable
+            where Code not in (
+                select Code 
+                from dbo.[Table]
+                where ProjectId='{0}'
+            )
+            ", projectId));
 
             //insert new table
             if (tables != null)
@@ -145,20 +145,20 @@ where Code not in (
                 foreach (JObject table in tables)
                 {
                     await db.ExecSqlAsync(string.Format(@"
-insert into dbo.[Table](Id, ProjectId, Code, Name, Note, Status)
-values('{0}', '{1}', '{2}', '', '{3}', 1)
-", _Str.NewId(), projectId, table["Code"].ToString(), table["Note"].ToString()));
+                    insert into dbo.[Table](Id, ProjectId, Code, Name, Note, Status)
+                    values('{0}', '{1}', '{2}', '', '{3}', 1)
+                    ", _Str.NewId(), projectId, table["Code"].ToString(), table["Note"].ToString()));
                 }
             }
 
             //update table.status(0/1)
             await db.ExecSqlAsync(string.Format(@"
-update a set 
-    Status=case when t.Code is null then 0 else 1 end
-from dbo.[Table] a
-left outer join #tmpTable t on t.Code=a.Code
-where a.ProjectId='{0}'
-", projectId));
+            update a set 
+                Status=case when t.Code is null then 0 else 1 end
+            from dbo.[Table] a
+            left outer join #tmpTable t on t.Code=a.Code
+            where a.ProjectId='{0}'
+            ", projectId));
             #endregion
 
             #region 6.insert/update dbo.Column from #tmpColumn
@@ -195,7 +195,7 @@ values(
     '', 0, '{4}',
     1)
 ",
-_Str.NewId(), col["TableId"].ToString(), col["Code"].ToString(), 
+_Str.NewId(), col["TableId"].ToString(), col["Code"].ToString(),
 Convert.ToByte(col["Nullable"]),
 col["Note"].ToString()
 ));
@@ -227,7 +227,7 @@ inner join dbo.[Table] t on t.Id=c.TableId
 inner join #tmpColumn tc on t.Code=tc.TableCode and c.Code=tc.Code
 where t.projectId='{0}'
 ", projectId));
-            #endregion
+        #endregion
 
         lab_exit:
             if (dbSrc != null)
@@ -249,67 +249,67 @@ where t.projectId='{0}'
             else
             {
                 _sqlGetTables = @"
-select 
-    Code=t.table_name,
-	Note=CASE WHEN p.value IS NULL THEN '' ELSE cast(p.value as varchar) end
-from Information_Schema.Tables t
-LEFT JOIN sys.extended_properties p ON p.major_id = object_id(t.table_schema + '.' + t.table_name) 
-	AND p.minor_id=0
-	AND p.name='MS_Description'     
-WHERE 1=1
---and TABLE_SCHEMA='dbo'
-AND TABLE_CATALOG='{0}'
-and table_type='BASE TABLE'
-ORDER BY t.table_name
-";
+                select 
+                    Code=t.table_name,
+                	Note=CASE WHEN p.value IS NULL THEN '' ELSE cast(p.value as varchar) end
+                from Information_Schema.Tables t
+                LEFT JOIN sys.extended_properties p ON p.major_id = object_id(t.table_schema + '.' + t.table_name) 
+                	AND p.minor_id=0
+                	AND p.name='MS_Description'     
+                WHERE 1=1
+                --and TABLE_SCHEMA='dbo'
+                AND TABLE_CATALOG='{0}'
+                and table_type='BASE TABLE'
+                ORDER BY t.table_name
+                ";
 
                 _sqlGetCols = @"
-SELECT 
-    Code=column_name, 
-	TableCode=table_name, 
-	DataType=data_type + (case when character_maximum_length is null then '' else '('+CONVERT(VARCHAR, character_maximum_length)+')' end),
-    Nullable=case when is_nullable = 'YES' then 1 else 0 end,
-	DefaultValue=case when i.COLUMN_DEFAULT is null then '' else replace(replace(i.COLUMN_DEFAULT,'(',''),')','') end,
-	Sort=ORDINAL_POSITION,
-    Note=ISNULL(( 
-		SELECT cast(value as varchar)
-        FROM ::FN_ListExtendedProperty(NULL, 'user', 'dbo', 
-			'table', table_name, 'column',
-			column_name)
-	), '') 
-FROM Information_Schema.Columns AS i
-WHERE 1=1
-AND TABLE_CATALOG='{0}'
-ORDER BY table_name, ORDINAL_POSITION
+                SELECT 
+                    Code=column_name, 
+                	TableCode=table_name, 
+                	DataType=data_type + (case when character_maximum_length is null then '' else '('+CONVERT(VARCHAR, character_maximum_length)+')' end),
+                    Nullable=case when is_nullable = 'YES' then 1 else 0 end,
+                	DefaultValue=case when i.COLUMN_DEFAULT is null then '' else replace(replace(i.COLUMN_DEFAULT,'(',''),')','') end,
+                	Sort=ORDINAL_POSITION,
+                    Note=ISNULL(( 
+                		SELECT cast(value as varchar)
+                        FROM ::FN_ListExtendedProperty(NULL, 'user', 'dbo', 
+                			'table', table_name, 'column',
+                			column_name)
+                	), '') 
+                FROM Information_Schema.Columns AS i
+                WHERE 1=1
+                AND TABLE_CATALOG='{0}'
+                ORDER BY table_name, ORDINAL_POSITION
 ";
             }
 
             /* MySql 語法(可執行)
                 case DbTypeEnum.MySql:
                     _sqlGetTables = @"
-SELECT 
-    t.table_name as Code, TABLE_COMMENT as Note
-FROM information_schema.tables t
-where 1=1
-and t.TABLE_SCHEMA='{0}'
-and t.table_type='BASE TABLE'
-ORDER BY t.table_name
-";
-
-                    _sqlGetCols = @"
-select 
-	table_name as TableCode, 
-    column_name as Code, 
-	Column_Type as DataType,
-    case when is_nullable = 'YES' then 1 else 0 end as Nullable,
-	case when COLUMN_DEFAULT is null then '' else replace(replace(COLUMN_DEFAULT,'(',''),')','') end as DefaultValue,
-	ORDINAL_POSITION as Sort,
-	Column_Comment as Note
-from information_schema.Columns
-where 1=1
-and TABLE_SCHEMA='{0}'
-order by table_name, ORDINAL_POSITION
-";
+                SELECT 
+                    t.table_name as Code, TABLE_COMMENT as Note
+                FROM information_schema.tables t
+                where 1=1
+                and t.TABLE_SCHEMA='{0}'
+                and t.table_type='BASE TABLE'
+                ORDER BY t.table_name
+                ";
+                
+                                    _sqlGetCols = @"
+                select 
+                	table_name as TableCode, 
+                    column_name as Code, 
+                	Column_Type as DataType,
+                    case when is_nullable = 'YES' then 1 else 0 end as Nullable,
+                	case when COLUMN_DEFAULT is null then '' else replace(replace(COLUMN_DEFAULT,'(',''),')','') end as DefaultValue,
+                	ORDINAL_POSITION as Sort,
+                	Column_Comment as Note
+                from information_schema.Columns
+                where 1=1
+                and TABLE_SCHEMA='{0}'
+                order by table_name, ORDINAL_POSITION
+                ";
                     break;
                 default:
                     break;
